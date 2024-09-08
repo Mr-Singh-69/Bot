38fb0a1290ab029d146cfa77e8574f94f7ea8047
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-09-08 21:50:33 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,9 +1,13 @@
 const http = require('http');
 
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Bot is alive!');
-  res.end();
-}).listen(3000, () => {
-  console.log('Keep-alive server is running on port 3000');
-});
+function keepAlive() {
+  http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Bot is alive!');
+    res.end();
+  }).listen(3000, () => {
+    console.log('Keep-alive server is running on port 3000');
+  });
+}
+
+module.exports = keepAlive; 
